d3132cd12a15bfb603f5c9a8b6f0815acc6272ef
Methamorphe <methamorphe@gmail.com>
2020-01-28 01:44:41 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const myInit = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+      "x-rapidapi-key": "af37a139cemsh5cc81dad7c007a6p196b65jsn2ee4fd320c31"
+    }
+  };
+
+  const request = async () => {
+    const response = await fetch(
+      "https://rawg-video-games-database.p.rapidapi.com/games",
+      myInit
+    );
+    const json = await response.json();
+    console.log(json);
+  };
+
+  request();
+
+  return <div>Hello World</div>;
+};
 
 export default App;
